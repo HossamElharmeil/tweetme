e0ce37f821702654ebc86a5787c4e43d1b348b16
HossamElharmeil <hossam.elharmil@gmail.com>
2021-05-08 06:43:52 +0200

    Redux: implement actions

---
+++ src/actions/shared.js
@@ -0,0 +1,15 @@
+import { getInitialData } from '../utils/api'
+import { receiveTweets } from './tweets'
+import { receiveUsers } from './users'
+import { setAuthedUser } from './authedUser'
+
+const AUTHED_ID = 'tylermcginnis'
+
+export const handleInitialData = () => dispatch => {
+    return getInitialData()
+        .then(({ users, tweets }) => {
+            dispatch(receiveTweets(tweets))
+            dispatch(receiveUsers(users))
+            dispatch(setAuthedUser(AUTHED_ID))
+        })
+}
