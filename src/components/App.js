ec4a78ca39e390f927d1b721b13f4bd1566cfebb
HossamElharmeil <hossam.elharmil@gmail.com>
2021-05-08 06:46:46 +0200

    App: initial data fetching

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(
+      handleInitialData()
+    )
+  }
+
   render() {
     return (
       <div>
-        Starter Code
+        {this.props.loading ? 'loading' : <Dashboard />}
       </div>
     )
   }
 }
 
-export default App
+const mapStateToProps = state => ({
+  loading: state.authedUser === null
+})
+
+export default connect(mapStateToProps)(App)
